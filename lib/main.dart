d838f8c4270815e2b60042a617b78033786cb83c
tawatchai7034 <tawatchaisunanta@gmail.com>
2022-03-26 15:42:57 +0700

    clone referenc

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,33 @@
-import 'package:cattle_db/screens/catPro_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import '../page/notes_page.dart';
 
-void main() {
-  runApp(const MyApp());
+Future main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  static final String title = 'Notes SQLite';
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: const CatProScreen(),
-    );
-  }
+  Widget build(BuildContext context) => MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: title,
+        themeMode: ThemeMode.dark,
+        theme: ThemeData(
+          primaryColor: Colors.black,
+          scaffoldBackgroundColor: Colors.blueGrey.shade900,
+          appBarTheme: AppBarTheme(
+            backgroundColor: Colors.transparent,
+            elevation: 0,
+          ),
+        ),
+        home: NotesPage(),
+      );
 }
